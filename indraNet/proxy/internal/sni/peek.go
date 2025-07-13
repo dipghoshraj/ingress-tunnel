5290869b553379c686e1b99e6e5d6e0754e4594e
dipghoshraj <dipghoshraj@gmail.com>
2025-07-13 12:31:45 +0530

    feat : update peek and start tunnel

--- indraNet/proxy/internal/sni/peek.go
+++ indraNet/proxy/internal/sni/peek.go
@@ -1,6 +1,9 @@
 package sni
 
-import "net"
+import (
+	"net"
+	"strings"
+)
 
 func PeekSNI(conn net.Conn) (string, net.Conn, error) {
 	buf := make([]byte, 512)
@@ -9,11 +12,15 @@ func PeekSNI(conn net.Conn) (string, net.Conn, error) {
 		return "", nil, err
 	}
 	serverName, err := ExtractHostFromHTTPRequest(buf[:n])
-	// serverName, err := ExtractSNIFromRequest(buf[:n]) this is for TLS SNI extraction in current context request is flowing over HTTP
+	// serverName, err := ExtractSNIFromRequest(buf[:n]) this is for TLS SNI extraction in current context request is flowing over
+	domain := strings.Split(serverName, ".")
+	if len(domain) == 0 {
+		return "", nil, nil // No SNI found
+	}
+
 	if err != nil {
 		return "", nil, err
 	}
 
-	return serverName, &ConnBuffer{buf: buf[:n], conn: conn}, nil
-
+	return domain[0], &ConnBuffer{buf: buf[:n], conn: conn}, nil
 }
