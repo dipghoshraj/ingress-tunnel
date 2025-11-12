57935bddaca5bb6b0571d639879f3bf239b9c713
dipghoshraj <dipghoshraj@gmail.com>
2025-11-12 20:30:47 +0530

    move region cehck

--- registry/pkg/memstore/mem.go
+++ registry/pkg/memstore/mem.go
@@ -46,3 +46,25 @@ type Capacity struct {
 	Storage   int32
 	Bandwidth int32
 }
+
+func (mem *MemStore) RegionExist(region string) *MemData {
+	mem.mu.RLock()
+	data, ok := mem.regions[region]
+	mem.mu.RUnlock()
+
+	if !ok {
+		mem.mu.Lock()
+		_, exists := mem.regions[region]
+		if !exists {
+			mem.regions[region] = &MemData{
+				Gateways: make(map[string]*GatewayData),
+				Agents:   make(map[string]*AgentData),
+				ranked:   btree.New(2),
+			}
+		}
+		data = mem.regions[region]
+		mem.mu.Unlock()
+	}
+	return data
+
+}
