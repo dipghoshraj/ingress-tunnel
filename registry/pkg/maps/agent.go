f8bcf717e8a745c85cb8de64596e1720be58aee5
dipghoshraj <dipghoshraj@gmail.com>
2025-11-12 20:31:10 +0530

    agent addition added

--- registry/pkg/maps/agent.go
+++ registry/pkg/maps/agent.go
@@ -3,11 +3,42 @@ package maps
 import (
 	"context"
 
+	memstore "github.com/dipghoshraj/ingress-tunnel/registry/pkg/memstore"
 	mapper "github.com/dipghoshraj/ingress-tunnel/registry/proto"
+	"github.com/google/uuid"
 )
 
 func (rpc *RPCMap) RegisterAgent(ctx context.Context, req *mapper.AgentConnectionRequest) (*mapper.AgentResponse, error) {
-	return &mapper.AgentResponse{}, nil
+
+	agentData := &memstore.AgentData{
+		AgentDomain:   req.AgentDomain,
+		AgentID:       uuid.New().String(),
+		GatewayDomain: req.Domain,
+		GatewayID:     req.GatewayId,
+	}
+
+	agent, gateway, err := rpc.MemStore.AddAgent("global", agentData)
+	if err != nil {
+		return &mapper.AgentResponse{
+			Error: &mapper.Error{
+				Code:    1,
+				Message: err.Error(),
+			},
+		}, nil
+	}
+	return &mapper.AgentResponse{
+		AgentId:       agent.AgentID,
+		AgentDomain:   agent.AgentDomain,
+		GatewayDomain: gateway.GatewayDomain,
+		GatewayId:     gateway.GatewayID,
+		Capacity: &mapper.Capacity{
+			Cpu:       gateway.Capacity.CPU,
+			Memory:    gateway.Capacity.Memory,
+			Storage:   gateway.Capacity.Storage,
+			Bandwidth: gateway.Capacity.Bandwidth,
+		},
+		Error: nil,
+	}, nil
 }
 
 func (rpc *RPCMap) ResolveGatewayForAgent(ctx context.Context, req *mapper.GatewayHandshake) (*mapper.MultipleGateways, error) {
