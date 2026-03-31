27005cb61ad784fc94827a3ad34ebf3843a461b7
dipghoshraj <dipghoshraj@gmail.com>
2026-04-01 00:52:22 +0530

    remove the indra net

--- indraNet/tunnel/agent-tunnel/internal/types/config.go
+++
@@ -1,9 +0,0 @@
-package types
-
-type ClientConfig struct {
-	GatewayURL  string
-	AgentDomain string
-	AgentID     string
-	Portforward string // Local port to forward
-	VFID        string // verifiable ID or similar identifier
-}
